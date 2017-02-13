1f2c0ddb721f0a851a053260a96a908309c4f080
Snazzythat <roman.andoni@gmail.com>
2017-02-13 09:20:22 -0500

    Made transition to sign up work

--- FitnessTracker_APP/FitnessTracker/www/js/controllers.js
+++ FitnessTracker_APP/FitnessTracker/www/js/controllers.js
@@ -1,11 +1,15 @@
 angular.module('starter.controllers', [])
 
 
-.controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('loginCtrl', ['$scope', '$state', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $state) {
 
+  $scope.switchTo = function(newPage)
+  {
+    $state.go(newPage);
+  };
 
 }])
 
