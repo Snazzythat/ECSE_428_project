152de924358486564da0976d6e82537e50d7f8bf
Snazzythat <roman.andoni@gmail.com>
2017-03-01 13:12:55 -0500

    Added distinguishment between user/trainer login codes and returns on webserver side

--- django_web/FitnessTracker/WebServices/views.py
+++ django_web/FitnessTracker/WebServices/views.py
@@ -17,18 +17,20 @@ class UserList(generics.ListCreateAPIView):
 
 @api_view(['POST'])
 def sign_up(request):
+    print 'POST SIGNUP request!'
     print 'Request data: ' + str(request.data)
     serializer = UserSerializer(data=request.data)
 
     if serializer.is_valid():
+        print 'New user signed up! New data is: ' + str(serializer.data)
         serializer.save()
         #Distinguish user and trainer after signup to be able to login  
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
-def login(request, username, password,type):
-    print 'GET request from: ' + username ' whose password is: ' + password + ' and whose account type is: ' + type
+def login(request, username, password, type):
+    print 'GET LOGIN request from: ' + username ' whose password is: ' + password + ' and whose account type is: ' + type
 
     #TODO: At get, we need to return 200 or 202 for USER and TRAINER
     # in order to be able to destinguish both. To do so, we need to first get the data from login request
@@ -40,5 +42,12 @@ def login(request, username, password,type):
     serializer = UserSerializer(user)
     #Distinguish user and trainer after login as well to be able to login  
     if user.password == password:
-        return Response(serializer.data)
+        #return Response(serializer.data) //old way just returning data
+        #Now need to return @login if we logged in as user or trainer
+        #Distinguish trainer and user with HTTP 200 and 202. 201 reserved for creation
+        if user.type == 'User':
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else if user.type == 'Trainer':
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
